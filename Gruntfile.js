41ba654c801fdca3af2ef2556fce321ebebccbff
Tudor Golubenco <tudor@packetbeat.com>
2014-12-08 12:15:39 +0100

    Added some styles.
    * Using grunt less & watch to compile less files.
    * Building bootstrap from less, for now without and modifications,
      but this will allow us to slim bootstrap down when needed.
    * All files get compiled in a single main.css file, which makes
      maintaining easy.
    * Using the default Bootswatch Flatly theme for now.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,22 +22,60 @@ module.exports = function(grunt) {
       }
     },
     'http-server': {
-      'dev': {
+      dev: {
         root: 'src/',
         port: 5062,
         ext: 'html',
         showDir: true,
-        autoIndex: true
+        autoIndex: true,
+        runInBackground: true
       },
+    },
+    less: {
+      dev: {
+        options: {
+          paths: ['src/styles'],
+        },
+        files: {
+          'src/styles/main.css': 'src/styles/main.less'
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: [
+          'src/styles/*.less',
+          'src/styles/theme/*.less'
+        ],
+        tasks: ['less'],
+        options: {
+          spawn: false
+        }
+      }
     }
   });
 
-  // Plugins
+  // Load grunt plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-http-server');
   grunt.loadNpmTasks('grunt-wiredep');
 
+  // Custom tasks
+  grunt.registerTask('dev', function() {
+    var tasks = [
+      'jshint',
+      'wiredep',
+      'less:dev',
+      'http-server',
+      'watch'
+    ];
+
+    grunt.task.run(tasks);
+  });
+
   // Default task(s)
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['dev']);
 
 };
