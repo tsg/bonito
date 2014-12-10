dc375840af8fe169e142f210784c5ae5f6e2731c
Tudor Golubenco <tudor@packetbeat.com>
2014-12-10 15:45:04 +0100

    more e2e tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -10,11 +10,28 @@
     });
 
     it('clicking Settings should go to Settings', function() {
-      element(by.css('a[href*="#settings"]')).click();
+      element(by.css('#bonito-navbar a[href*="#settings"]')).click();
 
       browser.getLocationAbsUrl().then(function(url) {
         expect(url.split('#')[1]).toBe('/settings');
       });
     });
+
+    it('clicking Services should go to settings/overview', function() {
+      element(by.css('#bonito-navbar a[href*="#services"]')).click();
+
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/services/overview');
+      });
+    });
+
+    it('clicking Map should go to settings/overview', function() {
+      element(by.css('#bonito-subnav a[href*="#services/map"]')).click();
+
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/services/map');
+      });
+    });
+
   });
 })();
