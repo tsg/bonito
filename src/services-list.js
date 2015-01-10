e13a7f3d65e4750fd712dc02e53211a58dd53aac
Tudor Golubenco <tudor@packetbeat.com>
2015-01-10 14:29:49 +0100

    Generate more interesting test data

--- src/services-list.js
+++ src/services-list.js
@@ -14,31 +14,87 @@
   app.factory('ServicesProxy', function() {
     var test_data = [];
 
-    this.generateData = function(min, max) {
-      var end = +(new Date());
-      var start = end - 120 * 1000;
-      var data = [];
-      for (var t = start; t < end; t+= 1000) {
-        data.push({
-          'ts': new Date(t),
-          'value': _.random(min, max)
-        });
+    this.generateData = function(lower, upper, curve) {
+      var end = +(new Date()),
+        start = end - 120 * 1000,
+        data = [],
+        t, min, max;
+
+      // something interesting to happen at the tipping point
+      var tipping_point = start + (end - start) / 3;
+      var locality = (end - start) / 25;
+
+      switch (curve) {
+      case 'jump-up':
+        max = upper;
+        min = lower;
+        for (t = start; t < end; t+= 1000) {
+          if (Math.abs(t - tipping_point) < locality) {
+            min = lower * 2;
+            max = upper * 2;
+          }
+          data.push({
+            'ts': new Date(t),
+            'value': _.random(min, max)
+          });
+        }
+        return data;
+
+      case 'jump-down':
+        max = upper;
+        min = lower;
+        for (t = start; t < end; t+= 1000) {
+          if (Math.abs(t - tipping_point) < locality) {
+            min = lower / 2;
+            max = upper / 2;
+          }
+          data.push({
+            'ts': new Date(t),
+            'value': _.random(min, max)
+          });
+        }
+        return data;
+
+      case 'peak':
+        for (t = start; t < end; t+= 1000) {
+          max = upper;
+          min = lower;
+          if (Math.abs(t - tipping_point) < locality) {
+            min = upper * 2.2;
+            max = upper * 2.5;
+          }
+          data.push({
+            'ts': new Date(t),
+            'value': _.random(min, max)
+          });
+        }
+        return data;
+
+      default:  // just random
+        for (t = start; t < end; t+= 1000) {
+          data.push({
+            'ts': new Date(t),
+            'value': _.random(lower, upper)
+          });
+        }
+        return data;
       }
-      return data;
     };
 
     var upper = 0, lower = 0;
     for (var i = 0; i < 81; i++) {
-      var avg = _.random(10, 1500);
+      var avg = _.random(10, 1500),
+        curves = ['random', 'peak', 'jump-up', 'jump-down'];
       // grow exponentially only until 30 to avoid
       // integer overflows
       lower = 100 * (1 << (i % 30));
-      upper = 100 * (1 << ((i %30) +1));
+      upper = 100 * (1 << ((i % 30) + 1));
+
 
       test_data.push({
         name: 'Service' + i,
         size: _.random(lower, upper),
-        values: this.generateData(lower, upper),
+        values: this.generateData(lower, upper, curves[_.random(curves.length)]),
         errors: _.random(0, i),
         rt_avg: avg,
         rt_50p: avg * 1.1,
