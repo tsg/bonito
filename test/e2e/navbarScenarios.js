7550696eaff79825c86f4e9f03c4278efa404239
Tudor Golubenco <tudor@packetbeat.com>
2015-01-31 20:44:10 +0100

    Split the e2e scenarios in multiple files

---
+++ test/e2e/navbarScenarios.js
@@ -0,0 +1,37 @@
+(function() {
+  'use strict';
+
+  describe('Bonito navigation scenarios', function() {
+    it('should redirect index.html to index.html#/services/overview', function() {
+      browser.get('/index.html');
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/services/overview');
+      });
+    });
+
+    it('clicking Settings should go to Settings', function() {
+      element(by.css('#bonito-navbar a[href*="#settings"]')).click();
+
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/settings');
+      });
+    });
+
+    it('clicking Services should go to settings/overview', function() {
+      element(by.css('#bonito-navbar a[href*="#services"]')).click();
+
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/services/overview');
+      });
+    });
+
+    it('clicking Map should go to settings/overview', function() {
+      element(by.css('#bonito-subnav a[href*="#services/map"]')).click();
+
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/services/map');
+      });
+    });
+
+  });
+})();
--- test/e2e/servicesScenarios.js
+++ test/e2e/servicesScenarios.js
@@ -2,37 +2,6 @@
   'use strict';
 
   describe('Bonito App', function() {
-    it('should redirect index.html to index.html#/services/overview', function() {
-      browser.get('/index.html');
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/services/overview');
-      });
-    });
-
-    it('clicking Settings should go to Settings', function() {
-      element(by.css('#bonito-navbar a[href*="#settings"]')).click();
-
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/settings');
-      });
-    });
-
-    it('clicking Services should go to settings/overview', function() {
-      element(by.css('#bonito-navbar a[href*="#services"]')).click();
-
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/services/overview');
-      });
-    });
-
-    it('clicking Map should go to settings/overview', function() {
-      element(by.css('#bonito-subnav a[href*="#services/map"]')).click();
-
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/services/map');
-      });
-    });
-
     describe('Services filtering', function() {
       beforeEach(function() {
         browser.get('/index.html#/services/overview?perRow=4');
